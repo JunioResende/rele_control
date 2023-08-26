8c8f4e65cfa4508125cd29de831511c1784be73a
JunioResende <junioresende23@gmail.com>
2023-08-26 17:53:41 -0300

    on_rele

--- src/main.cpp
+++ src/main.cpp
@@ -1,10 +1,21 @@
 #include <Arduino.h>
 
+const int relePin = 14;
+
 void setup() {
   Serial.begin(9600);
+
+  pinMode(relePin, OUTPUT);
+
+
+  digitalWrite(relePin, LOW);
 }
 
 void loop() {
-  Serial.println("Hello World!");
-  delay(1000);
+  digitalWrite(relePin, HIGH);
+  printf("Rele ligado\n");
+  delay(10000);
+  digitalWrite(relePin, LOW);
+  printf("Rele desligado\n");
+  delay(10000);
 }
